43e8d2b156998da5b20d119bbd36a0d5416946c4
aanant <aanant@umich.edu>
2021-01-09 00:52:33 +0530

    added env variable

--- api/app.js
+++ api/app.js
@@ -34,6 +34,14 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "..",'build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "..", "build", 'index.html'));
+  });
+}
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
