7fc89c0116456b856a1ef58bacadb96c86e1fc69
aanant <aanant@umich.edu>
2021-01-09 00:41:08 +0530

    added package.json

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, {useState, useEffect, useMemo} from "react";
+import Table from "./Table";
+import './App.css';
+import axios from "axios";
+
+function App() {
+    const members = useMemo(
+        () => [
+            {
+                Header: "Members",
+                columns: [
+                    {
+                        Header: "Name",
+                        accessor: "name"
+                    },
+                    {
+                        Header: "Username",
+                        accessor: "username",
+                    },
+                    {
+                        Header: "Admin",
+                        accessor: "admin"
+                    },
+                    {
+                        Header: "Active",
+                        accessor: "active"
+                    },
+                    {
+                        Header: "Two Factor enabled",
+                        accessor: "2fa"
+                    },
+                    {
+                        Header: "Emails",
+                        accessor: "email"
+                    },
+                    {
+                        Header: "Image",
+                        accessor: "image",
+                        Cell: ({ cell: {value}}) => (
+                                <img
+                                    src={value + "?token=5ff521dc-81d8-42db-9910-08651e89b4be"}
+                                    width={60}
+                                    alt={"No image present"}
+                                />
+                        )
+                    },
+                ]
+            },
+        ],
+        []
+    );
+    const projects = useMemo(
+        () => [
+            {
+                Header: "Projects",
+                columns: [
+                    {
+                        Header: "Name",
+                        accessor: "name"
+                    },
+                    {
+                        Header: "Team",
+                        accessor: "team",
+                    },
+                    {
+                        Header: "Stories",
+                        accessor: "stories"
+                    },
+                    {
+                        Header: "Followers",
+                        accessor: "followers",
+                        tipText: "tooltip text"
+                    },
+                    {
+                        Header: "Active",
+                        accessor: "active"
+                    }
+                ]
+            },
+        ],
+        []
+    );
+
+    const [memberData, setMemberData] = useState([]);
+    const [projectsData, setProjectData] = useState([])
+
+    const getData = async () => {
+        const result = await axios("/members", )
+        setMemberData(result.data);
+        const projectResult = await axios("/projects")
+        setProjectData(projectResult.data)
+    }
+    useEffect(() => {
+        getData()
+
+        const interval = setInterval(() => {
+            getData()
+        }, 60000)
+
+        return()=>clearInterval(interval)
+    }, [])
+  return (
+    <div className="App">
+    <Table columns={members} data={memberData} />
+    <Table columns={projects} data={projectsData} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Table.js
@@ -0,0 +1,118 @@
+import React, {useState} from "react";
+import {useTable, useFilters, useSortBy } from "react-table";
+import styled from "styled-components";
+
+const Styles = styled.div`
+  //padding: 1rem;
+  //
+  //table {
+  //  border-spacing: 0;
+  //  border: 1px solid black;
+  //
+  //  tr {
+  //    :last-child {
+  //      td {
+  //        border-bottom: 0;
+  //      }
+  //    }
+  //  }
+  //
+  //  th,
+  //  td {
+  //    margin: 0;
+  //    padding: 0.5rem;
+  //    border-bottom: 1px solid black;
+  //    border-right: 1px solid black;
+  //    position: relative;
+  //
+  //    :last-child {
+  //      border-right: 0;
+  //    }
+  //  }
+
+    /* My New CSS for Tooltip */
+    .new-tooltip span {
+      visibility: hidden;
+      width: 400px;
+      background-color: black;
+      color: #fff;
+      text-align: center;
+      border-radius: 6px;
+      padding: 5px 0;
+      position: absolute;
+      z-index: 1;
+      bottom: 80%;
+      left: 50%;
+      margin-left: -60px;
+      cursor: pointer;
+    }
+
+    .new-tooltip:hover span {
+      visibility: visible;
+    }
+  }
+`;
+
+export default function Table({ columns, data}) {
+    const [filterInput, setFilterInput] = useState("");
+
+    const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter
+} = useTable({
+    columns,
+    data
+},
+    useFilters,
+    useSortBy
+)
+    const handleFilterChange = e => {
+        const value = e.target.value || undefined;
+        setFilter("name", value);
+        setFilterInput(value)
+    };
+return (
+    <Styles>
+        <input
+            value={filterInput}
+            onChange={handleFilterChange}
+            placeholder={"Search name"}
+        />
+    <table {...getTableProps()}>
+        <thead>
+        {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps(column.getSortByToggleProps())}
+                    className={
+                        column.isSorted ? column.isSortedDesc ? "sort-desc" : "sort-asc" : ""
+                    }
+                    >{column.render("Header")}</th>
+                ))}
+            </tr>
+        ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+                <tr {...row.getRowProps()}>
+                    {row.cells.map((cell, index) => {
+                        if (cell.column.Header === "Followers") {
+                            return <td className="new-tooltip" {...cell.getCellProps()}>{cell.render("Cell")}{<span>{row.original.names}</span>}</td>
+                        }
+                        else{
+                            return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    }})}
+                </tr>
+            )
+        })}
+        </tbody>
+    </table>
+    </Styles>
+)
+}
